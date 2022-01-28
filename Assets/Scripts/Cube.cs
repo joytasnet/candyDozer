180052948eb89e2db35a00dae2159b4940ef9af0
mjpurin <mjpurin@gmail.com>
2022-01-28 14:21:24 +0900

    Testシーン作成

---
+++ Assets/Scripts/Cube.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(0f, 0f, 0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.Translate(0, 0, 0.1f);
+        transform.Rotate(0, 2f, 0);
+        transform.position = new Vector3(3f*Mathf.Cos(Time.time*3f), 3f*Mathf.Sin(Time.time*3f), Time.time*5f);
+
+    }
+}
