4f33e71bcfec19bd5ed337b36362ee9fce9f1d1e
mjpurin <mjpurin@gmail.com>
2022-01-31 15:25:25 +0900

    デストロイヤー追加

--- Assets/Scripts/CandyDestroyer.cs
+++ Assets/Scripts/CandyDestroyer.cs
@@ -5,8 +5,6 @@ using UnityEngine;
 public class CandyDestroyer : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other) {
-        if(other.gameObject.tag == "Candy") {
             Destroy(other.gameObject);
-        }
     }
 }
