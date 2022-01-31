c51d25c6724be8c9ea5977f0b3c05ca0be2b8707
mjpurin <mjpurin@gmail.com>
2022-01-31 14:00:10 +0900

    P198まで作成

---
+++ Assets/Scripts/CandyDestroyer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CandyDestroyer : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other) {
+        if(other.gameObject.tag == "Candy") {
+            Destroy(other.gameObject);
+        }
+    }
+}
